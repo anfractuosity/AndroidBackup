5032f28fd7662f2fc2a7aa68cabe3a4d1d002f56
anfractuosity <anfractuosity@anfractuosity.com>
2017-06-08 21:33:53 +0100

    bug fixes

--- backup.sh
+++ backup.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-rm /tmp/backup
+# Usage example: ./backup.sh /home/user/Phone
+
+if [ -d "$1" ]; then
+	echo "Using $1 for output directory"
+	OUTPUTDIR=$1
+else
+	echo "Invalid directory"
+	exit
+fi
+
+# Check phone exists
 
 adb shell "echo 1"
 if [ $? -ne 0 ]; then
@@ -8,26 +18,29 @@ if [ $? -ne 0 ]; then
 	exit
 fi
 
-EMAIL=$(adb shell dumpsys account | grep "\@" | head -n1 | sed "s/.*Account {name=//g;s/, type=com.google}//g" | tr -d '\r')
+# Get first email address of account
 
+EMAIL=$(adb shell dumpsys account | grep "\@" | head -n1 | sed "s/.*Account {name=//g;s/, type=com.google}//g" | tr -d '\r')
 MODEL=$(adb shell getprop ro.product.model | tr -d '\r')
 ADDR=$(adb shell cat /sys/class/net/wlan0/address | tr -d '\r')
 
 NAME="${EMAIL}_${MODEL}"
-mkdir $NAME
+mkdir "$OUTPUTDIR/$NAME"
 
-adb backup -apk -shared -all -f $NAME/$(date +"%Y-%m-%d_%H:%m").ab
+DATE=$(date +"%Y-%m-%d_%H:%M")
 
-adb shell content query --uri content://contacts/phones > $NAME/phones
-adb shell content query --uri content://settings/secure > $NAME/secure
-adb shell content query --uri content://settings/global > $NAME/global
+echo "Starting backup to $OUTPUTDIR/$NAME/$DATE.ab"
+adb backup -apk -shared -all -f "$OUTPUTDIR/$NAME/$DATE.ab"
 
-dd if=$NAME/$(date +"%Y-%m-%d_%H:%m").ab bs=24 skip=1 > /tmp/backup
+adb shell content query --uri content://contacts/phones > "$OUTPUTDIR/$NAME/phones_$DATE"
+adb shell content query --uri content://settings/secure > "$OUTPUTDIR/$NAME/secure_$DATE"
+adb shell content query --uri content://settings/global > "$OUTPUTDIR/$NAME/global_$DATE"
 
-zlib-flate -uncompress < /tmp/backup > /tmp/output
+# Get right part of file and uncompress 
+# See https://nelenkov.blogspot.de/2012/06/unpacking-android-backups.html for more info
 
-mv /tmp/output $NAME
+dd if="$OUTPUTDIR/$NAME/$DATE.ab" bs=24 skip=1 | zlib-flate -uncompress > "$OUTPUTDIR/$NAME/$DATE.tar"
 
-tar --force-local -xf $NAME/output -C $NAME
+tar --force-local -xf "$OUTPUTDIR/$NAME/$DATE.ab" -C "$OUTPUTDIR/$NAME"
 
 
